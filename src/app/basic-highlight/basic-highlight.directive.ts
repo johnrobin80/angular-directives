fbe3f2bc3d5f1ef1600904a772e390de9551702d
robin-john-menaa <robinprojectgcp@gmail.com>
2021-10-11 18:54:28 +0400

    Attribute directive session
    Attribute directive session

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,8 +1,13 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, OnInit } from "@angular/core";
 
 @Directive({
   selector: "[appBasicHighlight]",
 })
-export class BasicHighlightDirective {
-  constructor() {}
+export class BasicHighlightDirective implements OnInit {
+  stringData1: string;
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = "green";
+  }
 }
